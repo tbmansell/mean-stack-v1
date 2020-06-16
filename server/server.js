28ac87058f912089db334e34cabe17559415b036
toby.mansell1 <toby.mansell1@bp.com>
2020-06-16 22:09:47 +0100

    added CORS

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors');
 const bodyParser = require('body-parser')
 const ws = require('./websockets')
 
 const app = express()
+app.use(cors());
 app.use(bodyParser.json())
 app.use(require('./auth'))
 app.use('/api/posts', require('./controllers/api/posts.controller'))
